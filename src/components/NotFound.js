14dcd9c94ad2197e34ca033fe3be6dfb48ced4af
rabobahago <rabobahago@gmail.com>
2021-02-12 14:10:22 +0100

    routes added

---
+++ src/components/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react'
+const NotFound = () => {
+  return <h2>Not Found!11!</h2>
+}
+export default NotFound
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-//import StorePicker from './components/StorePicker'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import StorePicker from './components/StorePicker'
 import './css/style.css'
 import App from './components/App'
+import NotFound from './components/NotFound'
 
-render(<App />, document.querySelector('#main'))
+const Root = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={StorePicker} />
+          <Route path="/store/:storeId" component={App} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+render(<Root />, document.querySelector('#main'))

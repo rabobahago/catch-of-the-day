c93f37728e0f957b929ce84759433e2040445f40
rabobahago <rabobahago@gmail.com>
2021-02-21 12:24:49 +0100

    authentication added

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,12 +1,54 @@
 import React from 'react'
 import AddFishForm from './AddFishForm'
 import EditFishForm from './EditFishForm'
+import base from '../base'
 
 class Inventory extends React.Component {
+  state = {
+    uid: null,
+    owner: null,
+  }
+  authenticate = (provider) => {
+    console.log(`Try to login with ${provider}`)
+    base.signInWithEmailAndPassword(provider, this.authHandler)
+  }
+  authHandler = (err, authData) => {
+    console.log(authData)
+  }
+  renderLogin = () => {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p>Sign In to manage your store's inventory</p>
+        <button className="github" onClick={() => this.authenticate('github')}>
+          Log In with GitHub
+        </button>
+        <button
+          className="facebook"
+          onClick={() => this.authenticate('facebook')}
+        >
+          Log In with facebook
+        </button>
+      </nav>
+    )
+  }
   render() {
+    const logout = <button>Log Out</button>
+    if (!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          <p>Sorry, you aren't the owner of this store !!</p>
+          {logout}
+        </div>
+      )
+    }
     return (
       <div className="inventory">
         <h2>Inventory</h2>
+        {logout}
         {Object.keys(this.props.fishes).map((key) => (
           <EditFishForm
             key={key}
